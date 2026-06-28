02180b6ce3b0ec2b051d145f1fcd72f0704461bf
MarioYellowy <mario03eche@gmail.com>
2026-06-28 15:00:11 -0600

    Adjust dark mode by default in gtk

--- home/home.nix
+++ home/home.nix
@@ -29,4 +29,18 @@
     skyemu
     just
   ];
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome-themes-extra;
+    };
+  };
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
 }
