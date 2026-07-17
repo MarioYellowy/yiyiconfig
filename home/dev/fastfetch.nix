27940d3c59c49ca9dc55698bcf2b032ac2916c26
MarioYellowy <mario03eche@gmail.com>
2026-07-17 14:22:01 -0600

    feat(fastfetch): Implement logo

--- home/dev/fastfetch.nix
+++ home/dev/fastfetch.nix
@@ -1,5 +1,36 @@
 { ... }: {
   programs.fastfetch = {
     enable = true;
+
+    settings = {
+      ogo = {
+          source = "nixos_medium";
+          padding = {
+            right = 1;
+          };
+        };
+        display = {
+          size = {
+            binaryPrefix = "si";
+          };
+          color = "blue";
+          separator = "  ";
+        };
+        modules = [
+          {
+            type = "datetime";
+            key = "Date";
+            format = "{1}-{3}-{11}";
+          }
+          {
+            type = "datetime";
+            key = "Time";
+            format = "{14}:{17}:{20}";
+          }
+          "break"
+          "player"
+          "media"
+        ];
+    };
   };
 }
