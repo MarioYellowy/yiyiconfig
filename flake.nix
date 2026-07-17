8cf31f6c26f0d498e951a5b8e41bc92b8cbe9344
MarioYellowy <mario03eche@gmail.com>
2026-07-17 09:33:18 -0600

    feat(flake): Implement the unstable channel

--- flake.nix
+++ flake.nix
@@ -2,10 +2,11 @@
   description = "YiyiNixOS configuration with Noctalia";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -18,16 +19,23 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
   outputs =
     inputs@{
       self,
       nixpkgs,
+      nixpkgs-unstable,
       home-manager,
       hyprland,
       pixie-sddm,
       ...
     }:
+    let
+      system = "x86_64-linux";
+      pkgs-unstable = import nixpkgs-unstable {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      in
     {
       templates = {
               rust = {
@@ -39,9 +47,10 @@
             defaultTemplate = self.templates.rust;
 
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
+        inherit system;
+        specialArgs = { inherit inputs pkgs-unstable; };
         modules = [
+          ./configuration.nix
           {
             nixpkgs.config.allowUnfree = true;
             nixpkgs.overlays = [
@@ -49,16 +58,14 @@
                 surrealdb-bin = final.callPackage ./home/dev/surrealdb-bin.nix { };
                 surrealkit-bin = final.callPackage ./home/dev/surrealkit-bin.nix { };
               })
-
             ];
           }
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
+
+          home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.mario = import ./home/home.nix;
-            home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.extraSpecialArgs = { inherit inputs pkgs-unstable; };
           }
         ];
       };
