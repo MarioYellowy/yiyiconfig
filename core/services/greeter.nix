6f6457b915d76cb142dfb5ea7d5626a2679b8643
MarioYellowy <mario03eche@gmail.com>
2026-09-19 16:37:18 -0600

    Implement greeter

--- core/services/greeter.nix
+++ core/services/greeter.nix
@@ -2,6 +2,38 @@
   services.displayManager.noctalia-greeter = {
     enable = true;
     settings = {
+      appearance = {
+        scheme = "Synced";
+        password_style = "random";
+        hide_logo = true;
+        power_buttons_position = "bottom-right";
+        theme_mode = "dark";
+        font_family = "JetBrainsMono Nerd Font";
+      };
+
+      appearance.palette = {
+        primary = "#c6c6cd";
+        on_primary = "#2f3036";
+        secondary = "#c8c6c9";
+        on_secondary = "#303033";
+        tertiary = "#d0c3c8";
+        on_tertiary = "#362e32";
+        error = "#ffb4ab";
+        on_error = "#690005";
+        surface = "#141314";
+        on_surface = "#e5e2e2";
+        surface_variant = "#201f20";
+        on_surface_variant = "#c7c6cb";
+        outline = "#46464b";
+        shadow = "#000000";
+        hover = "#d0c3c8";
+        on_hover = "#362e32";
+      };
+
+      appearance.wallpaper = {
+        path = ../../assets/background.png;
+      };
+
       cursor = {
         theme = "Bibata-Modern-Classic";
         size = 20;
